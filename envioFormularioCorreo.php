5c7af8f3455558bffbd44555dd4e993d81ada21d
Rafael <rafael007@gmail.com>
2019-12-21 12:32:50 -0500

    My personal site

---
+++ envioFormularioCorreo.php
@@ -0,0 +1,13 @@
+<?php
+
+$destination = "rortizv@unicartagena.edu.co";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phoneNum = $_POST["phoneNum"];
+$message = $_POST["message"];
+$content = "Name: " . $name . . "\nEmail: " . $email . "\nPhone number: " . $phoneNum . "\nMessage: " . $message;
+mail($destination,"*** Message from your web site ***", $content);
+header("Location: thanks.html");
+
+?>
+
