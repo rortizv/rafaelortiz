c3abd08fdf087f14e0f1501ec5278f0bd3b8c0b9
Rafael <rafael007@gmail.com>
2020-01-12 13:35:15 -0500

    acomodar visual en iphone 6 plus

--- envioFormularioCorreo.php
+++ envioFormularioCorreo.php
@@ -5,7 +5,7 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $phoneNum = $_POST["phoneNum"];
 $message = $_POST["message"];
-$content = "Name: " . $name . . "\nEmail: " . $email . "\nPhone number: " . $phoneNum . "\nMessage: " . $message;
+$content = "Name: " . $name . "\nEmail: " . $email . "\nPhone number: " . $phoneNum . "\nMessage: " . $message;
 mail($destination,"*** Message from your web site ***", $content);
 header("Location: thanks.html");
 
